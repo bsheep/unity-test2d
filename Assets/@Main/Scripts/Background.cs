0b79ec796cf9e4325f0e54adce471fd5f342befd
bsheep <blacksheep.feather@gmail.com>
2021-08-17 14:41:15 +0900

    背景を追加

---
+++ Assets/@Main/Scripts/Background.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+// 背 景 を 制 御 す る ス ク リ プ ト
+public class Background : MonoBehaviour
+{
+    // 背 景 が ス ク ロ ー ル す る 速 さ
+    public Vector2 m_speed;
+    // 毎 フ レ ー ム 呼 び 出 さ れ る 関 数
+    private void Update()
+    {
+        // 背 景 の ス プ ラ イ ト を 表 示 す る 機 能 を 取 得 す る
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        // 背 景 の テ ク ス チ ャ を 表 示 す る マ テ リ ア ル を 取 得 す る
+        var material = spriteRenderer.material;
+        // 背 景 の テ ク ス チ ャ を ス ク ロ ー ル す る
+        material.mainTextureOffset += m_speed * Time.deltaTime;
+    }
+}
