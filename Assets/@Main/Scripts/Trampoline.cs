926833b9df32f1f3ff0c07fefbd8c8ace0ab5fbc
bsheep <blacksheep.feather@gmail.com>
2021-08-18 17:04:21 +0900

    add trampoline

---
+++ Assets/@Main/Scripts/Trampoline.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public float m_jumpHeight = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name.Contains("Player"))
+        {
+            var motor = collision.GetComponent<PlatformerMotor2D>();
+            motor.ForceJump(m_jumpHeight);
+
+            var animator = GetComponent<Animator>();
+            animator.Play("Jump");
+        }
+    }
+}
