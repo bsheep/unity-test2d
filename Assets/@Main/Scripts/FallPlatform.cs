311acf84571de66f92422c2f85edbcac6feee669
bsheep <blacksheep.feather@gmail.com>
2021-08-18 17:50:47 +0900

    動く床、落ちる床の追加

---
+++ Assets/@Main/Scripts/FallPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallPlatform : MonoBehaviour
+{
+    public float m_speed = 0.3f;
+    bool m_isHit;
+
+    private void Awake()
+    {
+        var motor = GetComponent<MovingPlatformMotor2D>();
+        motor.onPlatformerMotorContact += OnContact;
+    }
+
+    void OnContact(PlatformerMotor2D player)
+    {
+        if (player.IsFalling())
+        {
+            m_isHit = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_isHit)
+        {
+            var motor = GetComponent<MovingPlatformMotor2D>();
+            motor.velocity = Physics2D.gravity * m_speed;
+        }
+    }
+}
---
+++ Assets/@Main/Scripts/MovePlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public Vector3 m_distance = new Vector3(5, 0, 0);
+    public float m_speed = 0.5f;
+    Vector3 m_startPosition;
+    Vector3 m_endPosition;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_startPosition = transform.localPosition;
+        m_endPosition = m_startPosition + m_distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var t = Mathf.PingPong(Time.time * m_speed, 1);
+        var newPosition = Vector3.Lerp(m_startPosition, m_endPosition, t);
+
+        transform.localPosition = newPosition;
+    }
+}
