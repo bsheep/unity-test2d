d704d85f01dbcb0af2740d6035445213fb0e41c6
bsheep <blacksheep.feather@gmail.com>
2021-08-19 17:27:31 +0900

    add Goal

---
+++ Assets/@Main/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    bool m_isGoal;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!m_isGoal)
+        {
+            if (other.name.Contains("Player"))
+            {
+                var cameraShake = FindObjectOfType<CameraShaker>();
+                cameraShake.Shake();
+
+                m_isGoal = true;
+
+                var animator = GetComponent<Animator>();
+                animator.Play("Pressed");
+            }
+        }
+    }
+}
